0d4e0e5158ca4a0f0ae1b0719459049984afd200
Ricardo Guillen <nizzle@nizzledev.com>
2015-06-15 15:17:15 -0600

    Initial Commit

---
+++ Interview Homework Project/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Interview Homework Project
+//
+//  Created by Ricardo Guillen on 6/15/15.
+//  Copyright (c) 2015 Applaudo Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Interview Homework ProjectTests/Interview_Homework_ProjectTests.m
@@ -0,0 +1,40 @@
+//
+//  Interview_Homework_ProjectTests.m
+//  Interview Homework ProjectTests
+//
+//  Created by Ricardo Guillen on 6/15/15.
+//  Copyright (c) 2015 Applaudo Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Interview_Homework_ProjectTests : XCTestCase
+
+@end
+
+@implementation Interview_Homework_ProjectTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
